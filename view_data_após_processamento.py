039e8f3048c73f2bcaba1e8edec487f80eb8609b
karinyxD <kariny_abrahao@hotmail.com>
2026-01-29 19:26:39 -0300

    preprocessamento completo

--- view_data_após_processamento.py
+++ view_data_após_processamento.py
@@ -1,19 +1,16 @@
 import pandas as pd
 import preprocessamento as pp
  
-# 1. Chame sua função de pré-processamento
-# (Certifique-se de que as variáveis classes, limits, num_features, etc. estão definidas)
+# 1. Função de pré-processamento
 df_norm, df_real, labels = pp.preprocessing_pts()
 
 # 2. Preparação para o CSV de Visualização
-# Vamos dar nomes diferentes às colunas para não confundir
-# Ex: "TSH" vira "TSH_real" e "TSH_zscore"
-
-# Cria uma cópia para não alterar os dados originais que serão usados no cálculo
+# Ex: "TSH_real" e "TSH_zscore"
+# Dados reais vs dados normalizados 
 df_visual = df_real.add_suffix('_real')        # Adiciona sufixo nos valores reais
 df_norm_visual = df_norm.add_suffix('_zscore') # Adiciona sufixo nos valores normalizados
 
-# 3. Juntar tudo em um único DataFrame (lado a lado)
+# 3. Juntar tudo em um único DataFrame 
 # axis=1 significa juntar colunas, index=True garante que os pacientes alinhem corretamente
 df_final_visual = pd.concat([df_visual, df_norm_visual, labels], axis=1)
 
@@ -23,15 +20,14 @@ cols = ['severity_label']
 for col in df_real.columns:
     cols.append(f"{col}_real")
     cols.append(f"{col}_zscore")
-
 try:
     df_final_visual = df_final_visual[cols]
 except KeyError:
     pass 
 
 # 5. Salvar o CSV
-nome_arquivo = "dados_processados_para_validacao.csv"
+nome_arquivo = "dados_processados.csv"
 df_final_visual.to_csv(nome_arquivo, index=True)
 
 print(f"Arquivo '{nome_arquivo}' gerado com sucesso!")
-print("Abra este arquivo para conferir se o Z-Score faz sentido comparado ao valor real.")
+print("Para conferir se o Z-Score faz sentido comparado ao valor real.")
